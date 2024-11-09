32245165b7d5404ae778958b5f620264ffe053b2
CreepyMemes <mjouins@gmail.com>
2024-11-10 00:56:51 +0100

    minor log crap

--- src/SimpleSerial.cpp
+++ src/SimpleSerial.cpp
@@ -223,7 +223,7 @@ bool SimpleSerial::_sender_retry(const Command cmd) {
         }
 
         if (attempt != _max_retries - 1) {
-            ESP_LOGW(TAG, "Attempt: %d, Retrying to send...\n", attempt + 2);
+            ESP_LOGW(TAG, "Attempt: %d, Retrying to send...", attempt + 2);
         }
 
         delay(1); // To avoid flooding the CPU
